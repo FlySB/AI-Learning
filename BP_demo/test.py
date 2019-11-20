2cfec47781f9f4c5cb055d0ec39836bb3018c72c
FlySB <1538925211@qq.com>
2019-11-20 20:13:01 +0800

    NewBP

--- BP_demo/test.py
+++ BP_demo/test.py
@@ -2,7 +2,7 @@ from NewBP import BP
 import numpy as np
 np.set_printoptions(suppress=True)
 
-nn = BP(6, 7, 1)
+
 trainX = [[1, 1, 1, 1, 1, 1],
           [2, 1, 2, 1, 1, 1],
           [2, 1, 1, 1, 1, 1],
@@ -14,10 +14,10 @@ trainX = [[1, 1, 1, 1, 1, 1],
           [3, 1, 1, 3, 3, 1],
           [1, 1, 2, 2, 2, 1]]
 X = np.array(trainX)
-trainY = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
-y = (np.array(trainY)).T
 
-nn.fit(X, y, 0.2, 10000)
+trainY = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+Y = (np.array(trainY)).T
+
 testX = [[1, 1, 2, 1, 1, 1],
         [3, 1, 1, 1, 1, 1],
         [2, 2, 1, 1, 2, 1],
@@ -25,5 +25,21 @@ testX = [[1, 1, 2, 1, 1, 1],
         [3, 3, 3, 3, 3, 1],
         [3, 1, 1, 3, 3, 2],
         [1, 2, 1, 2, 1, 1]]
+testY = [0, 0, 0, 1, 1, 1, 1]
+testY = np.array(testY)
 
-print(nn.predict(X))
+E = 0;
+frequency = 10
+
+for i in range(frequency):
+    nn = BP(6, 7, 1)
+    nn.fit(X, Y, 0.2, 10000)
+    predictY = nn.predict(np.array(testX))
+    predictY = predictY.T
+    print("第"+str(i+1)+"次预测结果：")
+    print(predictY)
+    E += np.sum((testY - predictY)*(testY - predictY))/len(testY)
+
+print("10次随机BP后的平均方差：")
+print(E/frequency)
+
--- BP_demo/test2.py
+++ BP_demo/test2.py
@@ -1,8 +1,3 @@
-import numpy as np
-temp = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
-
-x = np.array(temp)
-
-y = np.ones((x.shape[0], x.shape[1]+1))
-p =x[:, :2]
-print(p)
+import random
+x = [[random.random() for i in range(3)] for j in range(4)]
+print(x)
